80159c082473fefd5a85bbcee003c25834bda1b7
Jelle Monen <jelle.monen@hotmail.com>
2019-05-29 14:32:32 +0200

    Making sure the button actually works

--- pages/backup.js
+++ pages/backup.js
@@ -1,28 +1,28 @@
-// add event handler
 function submitComment() {
-    // gather data
+
+    //gather data
     const inputField = document.getElementById('name')
     const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
-    
-    // create the elements you need
+
+    //create elements needed
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-  
-    // adjust the elements we created
+
+    //adjusting elements
     h3.innerHTML = `${name} said:`
     p.innerHTML = msg
-    comment.classList.add('comment')
+    comment.classList.add(`comment`)
     comment.appendChild(h3)
     comment.appendChild(p)
-  
-    // display the elements on the page
-    const commentSection = document.getElementById('comments')
+
+   
+    const commentSection = getElementById('comments')
     commentSection.appendChild(comment)
-  
-    // reset form values
-    inputField.value = null
-    textArea.value = null
+    
+    inputField = null
+    textArea = null
   }
+  
